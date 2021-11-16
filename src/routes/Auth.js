b35d5a3e80b520e1994f0c0eb4ea7fb81b77276a
kakasleki <kakasleki@naver.com>
2021-11-17 00:12:30 +0900

    2021.11.17 LSK 기본 로그인 완료

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,60 @@
+import { authService } from "fbase";
+import { useState } from "react";
+
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
+    const onChange = (event) => {
+        const {
+            target: {name, value},
+        } = event;
+
+        if(name === "email") {
+            setEmail(value);
+        } else if(name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            let data;
+
+            if(newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
     return (
         <>
-            <span>Auth</span>
+            <div>
+                <form onSubmit={onSubmit}>
+                    <input type="email" name="email" placeholder="Email" required value={email} onChange={onChange} />
+                    <input type="password" name="password" placeholder="Password" required value={password} onChange={onChange} />
+                    <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                    {error}
+                </form>
+                <span onClick={toggleAccount}>
+                    {newAccount ? "Sign In" : "Create Account"}
+                </span>
+                <div>
+                    <button>Continue with Google</button>
+                    <button>Continue with Github</button>
+                </div>
+            </div>
         </>
     );
 };
