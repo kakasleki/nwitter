455eecd31ce3381265b6b5eb654bcc8cd5cedba9
kakasleki <kakasleki@naver.com>
2021-11-19 00:10:48 +0900

    2021.11.19 LSK 트윗 등록 조회기능 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,7 +4,7 @@ import Home from "routes/Home";
 import Navigation from "./Navigation";
 import Profile from "../routes/Profile";
 
-const AppRouter = ({isLoggedIn}) => {
+const AppRouter = ({isLoggedIn, userObj}) => {
 
     return (
         <Router>
@@ -13,7 +13,7 @@ const AppRouter = ({isLoggedIn}) => {
             <Routes>
                 { isLoggedIn ? (
                     <>
-                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/" element={<Home userObj={userObj} />} />
                         <Route exact path="/profile" element={<Profile />} />
                     </>
                 ) : (
